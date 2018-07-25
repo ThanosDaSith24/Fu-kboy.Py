946956e34d447b5f52a75956d3b1b9a7444aef0f
ThanosDaSith24 <41706285+ThanosDaSith24@users.noreply.github.com>
2018-07-25 14:35:13 -0700

    Add files via upload

---
+++ fuckboy.py
@@ -0,0 +1,9 @@
+print "How many pairs did you cook?",
+number = raw_input()
+print "How many pairs got cancelled?"
+number1 = raw_input()
+print "How much did you spend on proxies?"
+number3 = raw_input()
+
+print "So you cooked %r pairs and got cancelled %r times after staying up 36+ hours and spending %r on proxies like a real fuckboy." % (
+	number, number1, number3) 
